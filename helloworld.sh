98e8af1a9b343025e44694082302273000fe0e21
VardhanNS <vardhan@edureka.co>
2017-10-04 19:57:23 +0530

    This is the third line-#3

--- helloworld.sh
+++ helloworld.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 echo Hello World!
 echo Hi from edureka!
+echo Your instructor is Vardhan!
--- helloworld.sh~
+++ helloworld.sh~
@@ -1,4 +1,3 @@
 #! /bin/bash
 echo Hello World!
 echo Hi from edureka!
-echo Your instructor is Vardhan!
